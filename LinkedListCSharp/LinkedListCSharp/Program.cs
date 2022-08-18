936676307412171ad1ad1ad22208f49c39c5a3ae
Shayan Fahmi <shayan.fahmi@gmail.com>
2022-08-18 22:07:15 +0530

    Print LinkedList

--- LinkedListCSharp/LinkedListCSharp/Program.cs
+++ LinkedListCSharp/LinkedListCSharp/Program.cs
@@ -6,7 +6,42 @@ namespace LinkedListCSharp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var a = new Node('A');
+            var b = new Node('B');
+            var c = new Node('C');
+            var d = new Node('D');
+            var e = new Node('E');
+            a.next = b;
+            b.next = c;
+            c.next = d;
+            d.next = e;
+            //printLinkedList(a);
+            printLinkedListRecursively(a);
+        }
+
+        public static void printLinkedList(Node head) {
+            var curr = head;
+            while (curr != null)
+            {
+                Console.WriteLine(curr.val);
+                curr = curr.next;
+            }
+        }
+
+        public static void printLinkedListRecursively(Node head) {
+            if (head == null) return;
+            Console.WriteLine(head.val);
+            printLinkedListRecursively(head.next);
+        }
+    }
+
+    public class Node {
+        public char val;
+        public Node next;
+
+        public Node(char val) {
+            this.val = val;
+            this.next = null;
         }
     }
 }
